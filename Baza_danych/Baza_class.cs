6f530f6e5e675cc815868287f502d89d6eab9fd7
Jace Ada <jacko785@outlook.com>
2018-05-18 17:32:38 +0200

    Zmia

---
+++ Baza_danych/Baza_class.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza_danych
+{
+    class Baza_class
+    {
+        static string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=baza;SslMode=none;";
+        MySqlConnection conn = new MySqlConnection(connectionString);
+
+        public void connect_data()
+        {
+            
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Udalo sie poloczyc z bazo");
+            }
+            catch
+            {
+                Console.WriteLine("Nie udalo sie poloczyc z bazą");
+                conn.Close();
+            }
+
+
+        }
+        public void show_data()
+        {
+            string query = "select * from user";
+            MySqlCommand command = new MySqlCommand(query, conn);
+
+            MySqlDataReader read;
+            read = command.ExecuteReader();
+
+            try
+            {
+                if (read.HasRows)
+                {
+                    while (read.Read())
+                    {
+                        int id = read.GetInt32(0);
+                        string name = read.GetString(1);
+                        string passwd = read.GetString(2);
+
+                        Console.WriteLine("id= {0} name= {1} passwd= {2}", id, name, passwd);
+
+                    }
+
+                }
+                conn.Close();
+            }
+            catch
+            { 
+            
+                Console.WriteLine("Nie udało sie pokazac danych");
+                conn.Close();
+            }
+
+        }
+
+    }
+}
---
+++ Baza_danych/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Baza_danych
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Baza_class connect = new Baza_class();
+
+            connect.connect_data();
+            connect.show_data();
+
+            string namee = "Kamil";
+            string pass = "Kamil12";
+            string insert_query = "INSERT INTO user (Name,Passwd) VALUES ('"+namee+"','"+pass+"')";
+
+            Console.WriteLine(insert_query);
+            
+            Console.ReadKey();
+        }
+    }
+}
